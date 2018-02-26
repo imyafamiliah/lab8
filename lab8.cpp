d294bbd68f46d2006321a5e09e338dd47b2c48b9
Timur Ivanov <imyafamiliah@gmail.com>
2018-02-26 09:36:25 +0900

    pochti sdelal 2

--- lab8.cpp
+++ lab8.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
 #include <dirent.h>
 #include <sys/types.h>
-
+#include <sys/stat.h>
+#include <string.h>
 using namespace std;
 
-int main()
+void dfs()
 {
 	DIR* dp = opendir(".");
 	struct dirent *res;
 
 	while ((res = readdir(dp)) != NULL) {
-    	cout << res->d_name << "\n";
+        if(strcmp(res->d_name,"."==0) || strcmp(res->d_name,"..")==0) continue ;
+        struct stat info;
+    	stat(res->d_name, &info);
+    	if (S_ISDIR(info.st_mode)){
+            cout << "DIR";
+            fcount++;
+    	}else{
+    	size +=info.st.size;
+    	count++;
+    	}
 	}
     closedir(dp);
 
-    return 0;
 }
 
+int main()
+{
+    dfs();
+    cout <<count << "files, "<< fcount << "dirs, " << size << "bytes.\n";
+    return 0;
+}
